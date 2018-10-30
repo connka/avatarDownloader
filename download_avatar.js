da081047cd54870e056231df73eb7ae57c19a89d
Kate Connolly <katherine.d.connolly@gmail.com>
2018-10-30 18:23:22 +0000

    find a single url

--- download_avatar.js
+++ download_avatar.js
@@ -1,3 +1,30 @@
-var request = require('request');
+var request = require("request");
+var myToken = require("./secret.js");
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+console.log("Welcome to the GitHub Avatar Downloader!");
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  //var contributors = JSON.parse(repoOwner).avatar_url;
+  var options = {
+    url:
+      "https://api.github.com/repos/" +
+      repoOwner +
+      "/" +
+      repoName +
+      "/contributors",
+    headers: {
+      "User-Agent": "request",
+      Authorization: "token" + myToken.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body)[0].avatar_url);
+  });
+}
+
