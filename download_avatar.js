593508b6cb922528740c4c69f5ae25eab5d1aa67
Kate Connolly <katherine.d.connolly@gmail.com>
2018-10-30 19:24:15 +0000

    Step 7

--- download_avatar.js
+++ download_avatar.js
@@ -4,27 +4,40 @@ var myToken = require("./secret.js");
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+    console.log("Errors:", err);
+    console.log("Result:", result);
 });
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  //var contributors = JSON.parse(repoOwner).avatar_url;
-  var options = {
+    //var contributors = JSON.parse(repoOwner).avatar_url;
+    var options = {
     url:
-      "https://api.github.com/repos/" +
-      repoOwner +
-      "/" +
-      repoName +
-      "/contributors",
+    "https://api.github.com/repos/" +
+    repoOwner +
+    "/" +
+    repoName +
+    "/contributors",
     headers: {
-      "User-Agent": "request",
-      Authorization: "token" + myToken.GITHUB_TOKEN
+    "User-Agent": "request",
+    Authorization: "token" + myToken.GITHUB_TOKEN
     }
-  };
+};
 
-  request(options, function(err, res, body) {
-    cb(err, JSON.parse(body)[0].avatar_url);
-  });
+function parse (body) {
+    return JSON.parse(body);
 }
 
+request(options, function(err, res, body) { 
+    let allInfo = parse(body);
+    for (let item in allInfo) {
+        if (allInfo[item].hasOwnProperty('avatar_url')) {
+            console.log("Avatar URL for " + allInfo[item].login + "is: " + allInfo[item].avatar_url)
+        } else {
+            console.log("No Avatar URL found")
+        }
+        
+    }
+
+    })
+//        cb(err, JSON.parse(body)[0].avatar_url);
+    }
